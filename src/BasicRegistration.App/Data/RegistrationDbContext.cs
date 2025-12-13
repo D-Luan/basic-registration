78136646d672cd9c4e3ee7aa08ef8035d023eb17
d-luan <dluan1285@gmail.com>
2025-12-13 15:01:21 -0300

    feat: implements Registration model and DbContext settings

---
+++ src/BasicRegistration.App/Data/RegistrationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BasicRegistration.App.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicRegistration.App.Data;
+
+public class RegistrationDbContext : DbContext
+{
+    public RegistrationDbContext(DbContextOptions<RegistrationDbContext> options)
+        : base(options)
+    {
+    }
+
+    DbSet<Registration> Registrations { get; set; } = null!;
+}
---
+++ src/BasicRegistration.App/Models/Registration.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Principal;
+
+namespace BasicRegistration.App.Models;
+
+public class Registration
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateOnly BirthDate { get; set; }
+    public string Cpf { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+    public DateTime RegistrationDate { get; set; }
+}
